0edd021c2dac9ceaaf2d50641f82da1fadb8e090
cap3r5 <cap3r5@gmail.com>
2019-05-13 14:15:11 -0400

    small spelling issue
    Great DH for my lock. There was just a small spelling issue on vacation mode.

--- devicetypes/ethayer/zwave-lock-schlage.src/zwave-lock-schlage.groovy
+++ devicetypes/ethayer/zwave-lock-schlage.src/zwave-lock-schlage.groovy
@@ -138,7 +138,7 @@ metadata {
           displayDuringSetup: false,  options: ["Extreme", "High", "Medium", "Moderate", "Low"]
 
 		input name: "autoLock", type: "bool", title: "Auto Lock", description: "Enable Auto Lock?", required: false, displayDuringSetup: false
-		input name: "vacationMode", type: "bool", title: "Vataction Mode", description: "Enable Vacation Mode?", required: false, displayDuringSetup: false
+		input name: "vacationMode", type: "bool", title: "Vacation Mode", description: "Enable Vacation Mode?", required: false, displayDuringSetup: false
 		input name: "lockLeave", type: "bool", title: "Lock & Leave", description: "Enable Lock & Leave?", required: false, displayDuringSetup: false
 		input name: "localControl", type: "bool", title: "Local Control", description: "Enable Local Control?", required: false, displayDuringSetup: false
 		input name: "pinLength", type: "number", title: "Pin Length", description: "Changing will delete all codes", range: "4..8", required: false, displayDuringSetup: false
